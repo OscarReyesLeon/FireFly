6c09a47c155dffd37d919c5d76fef1553f09bcc1
Oscar Reyes <oscar@mercax.com>
2022-01-30 21:29:39 -0600

    corregir iva del lado izquierdo

--- cmp/models.py
+++ cmp/models.py
@@ -59,7 +59,7 @@ class ComprasEnc(ClaseModelo):
         if self.sub_total == None  or self.descuento == None:
             self.sub_total = 0
             self.descuento = 0
-        self.total = self.sub_total - self.descuento
+        self.total = self.sub_total + ( self.sub_total * self.descuento )
         super(ComprasEnc,self).save()
 
     class Meta:
